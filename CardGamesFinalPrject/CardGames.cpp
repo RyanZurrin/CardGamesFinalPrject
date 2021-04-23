c6a2fc3b0bc1b9a15d6ef812ab3e67d0d3e3b5f1
drathnis <drathnis@gmail.com>
2021-04-22 22:01:49 -0400

    getting started

--- CardGamesFinalPrject/CardGames.cpp
+++ CardGamesFinalPrject/CardGames.cpp
@@ -1,19 +1,19 @@
 #include "Crazy8.h"
 #include <iostream>
-using namespace std;
+
 
 int main(){
 
 
 	int game = 1;
 	int playerCount = 2;
-	//cout << "What Game?";
+	cout << "What Game?";
 	//cin >> game;
 
-
 	if (game==1){
-	//	cout << "players?" << endl;
-	//	cin >> playerCount;
+		//Julian's Game Crazy Eights
+		//cout << "players?" << endl;
+		//cin >> playerCount;
 		CrazyEight c8(playerCount);
 
 	} else	if (game == 2){
