a28f18d1cbac65b2033dd584cb1fabb530e66bbc
Ryan Zurrin <rzurrin@gmail.com>
2021-04-23 06:36:05 -0400

    trying to resolve merge conflicts

--- CardGamesFinalPrject/Crazy8.h
+++ CardGamesFinalPrject/Crazy8.h
@@ -1,48 +1,81 @@
 #pragma once
 #include "deckOfCards.h"
-using namespace std;
+#include "player.h"
 
-class CrazyEight: private deckOfCards{
+
+
+
+class CrazyEight{
 public:
 	CrazyEight(int numPlayers);
 	~CrazyEight();
 
 private:
-	deckOfCards *deck;
-	int numplayers;
+	deckOfCards* deck;
+	int numPlayers;
 	void play();
+	Player *players;
+	void dealCards();
+
 };
 
 void CrazyEight::play(){
+	cout << "Play" << endl;
+	dealCards();
+	players[0].showAllCards();
+}
+
+void CrazyEight::dealCards(){
+	cout << "Deal" << endl;
+	for (int p = 0; p < numPlayers; p++)		{
+	//	cout << "Player " << p << endl;
+		for (int i = 0; i < players[p].getHandSize(); i++){
+		//	cout << "Add" << endl;
+			players[0].addCard(deck->takeCard());
+		}
+
+	}
 
 }
 
 CrazyEight::CrazyEight(int numPlayers){
-	
-	deck = NULL;
-	//deckOfCards deck(1);
-	//deck = new deckOfCards(1);
+	this->numPlayers = numPlayers;
 
-	if (numPlayers == 1){
+	players = new Player[numPlayers];
+	
+	int handSize =7;
+	int numDecks =1;
+
+	if (this->numPlayers == 1){
 		cout << "Sorry you cant play by yourself at the moment" << endl;
-	} else if (numPlayers == 2){
-		deck = new deckOfCards(1);
-	} else if (numPlayers < 20){
-		deck = new deckOfCards(2);
+	} else if (this->numPlayers == 2){
+		numDecks = 1;
+		handSize = 7;
+	} else if (this->numPlayers < 20){
+		numDecks = 2;
+		handSize = 5;
 	} else{
-		deck = new deckOfCards(3);
+		numDecks = 3;
+		handSize = 5;
 	}
 
-	deck->displayDeck();
+	deck = new deckOfCards(numDecks);
+	
+	deck->shuffleDeck();
 
 
 
+	for (int i = 0; i < numPlayers; i++)		{
+		players[i].createPlayer(handSize , 52*numDecks);
+	}
+
+	play();
 
 
 }
 
 CrazyEight::~CrazyEight(){
 
-
+	delete [] players;
 	delete deck;
 }
